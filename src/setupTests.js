a35d13cdb60d30cb6ba02469260a647bef9d66d7
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-12 04:08:11 +0200

    test(demo): configure snapshot tests on the project

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
 import { configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
+import { createSerializer } from "enzyme-to-json"
 
 configure({ adapter: new Adapter() })
+
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }))
