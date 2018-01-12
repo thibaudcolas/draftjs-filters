5c5936dd6d3ddeba43cb5143f2f50bf698156435
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-12 19:35:39 +0200

    feat(api): expose all filters to package consumers

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,18 @@
+import {
+  preserveAtomicBlocks,
+  resetBlockDepth,
+  resetBlockType,
+} from "./filters/blocks"
+import { filterInlineStyle } from "./filters/styles"
+import { resetAtomicBlocks, filterEntityType } from "./filters/entities"
 import { filterEditorState } from "./filters/editor"
 
-export { filterEditorState }
+export {
+  preserveAtomicBlocks,
+  resetBlockDepth,
+  resetBlockType,
+  filterInlineStyle,
+  resetAtomicBlocks,
+  filterEntityType,
+  filterEditorState,
+}
--- src/lib/index.test.js
+++ src/lib/index.test.js
@@ -14,5 +14,11 @@ const pkg = require("../../package.json")
  * Makes sure the API shape is validated against.
  */
 describe(pkg.name, () => {
+  it("preserveAtomicBlocks", () => expect(preserveAtomicBlocks).toBeDefined())
+  it("resetBlockDepth", () => expect(resetBlockDepth).toBeDefined())
+  it("resetBlockType", () => expect(resetBlockType).toBeDefined())
+  it("filterInlineStyle", () => expect(filterInlineStyle).toBeDefined())
+  it("resetAtomicBlocks", () => expect(resetAtomicBlocks).toBeDefined())
+  it("filterEntityType", () => expect(filterEntityType).toBeDefined())
   it("filterEditorState", () => expect(filterEditorState).toBeDefined())
 })
