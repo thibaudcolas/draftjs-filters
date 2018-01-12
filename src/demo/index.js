6530b63fb82f5b991dca7d67ffaac4a023bc7ee9
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-12 15:44:13 +0200

    chore(demo): add basic styles to the demo site

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,12 +2,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import "normalize.css"
+
+import "./utils/elements.css"
+import "./utils/typography.css"
+import "./utils/layout.css"
+import "./utils/objects.css"
+
 import "prismjs/themes/prism.css"
 import "draft-js/dist/Draft.css"
 
-import "./index.css"
+import "./components/header.css"
+import "./components/page-nav.css"
 
-import App from "./App"
+import "./utils/utilities.css"
+
+import App from "./components/App"
 
 const mount = document.getElementById("root")
 
