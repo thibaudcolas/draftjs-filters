e1a1f7c748293938097c8038ad929dbf3663dc2a
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-13 17:09:56 +0200

    chore(demo): add simple Link implementation to demo site

--- src/demo/components/Link.test.js
+++ src/demo/components/Link.test.js
@@ -1,12 +1,52 @@
 import React from "react"
 import { shallow } from "enzyme"
-import { EditorState, ContentState, convertFromHTML } from "draft-js"
+import {
+  EditorState,
+  ContentState,
+  convertFromHTML,
+  convertFromRaw,
+} from "draft-js"
 
 import Link, { linkStrategy } from "./Link"
 
 describe("Link", () => {
   it("renders", () => {
-    expect(shallow(<Link>Test</Link>)).toMatchSnapshot()
+    const contentState = convertFromRaw({
+      entityMap: {
+        "0": {
+          type: "LINK",
+          mutability: "MUTABLE",
+          data: {
+            url: "www.example.com",
+          },
+        },
+      },
+      blocks: [
+        {
+          key: "6i47q",
+          text: "NA link doc",
+          type: "unstyled",
+          depth: 0,
+          inlineStyleRanges: [],
+          entityRanges: [
+            {
+              offset: 3,
+              length: 4,
+              key: 0,
+            },
+          ],
+          data: {},
+        },
+      ],
+    })
+
+    expect(
+      shallow(
+        <Link contentState={contentState} entityKey="1">
+          Test
+        </Link>,
+      ),
+    ).toMatchSnapshot()
   })
 })
 
