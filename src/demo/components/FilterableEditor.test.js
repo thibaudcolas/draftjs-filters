19e62eac3b4fc634409edfee80e437315efa8653
Thibaud Colas <thibaudcolas@gmail.com>
2018-02-06 21:41:19 +0200

    test(demo): add missing tests for demo editor

--- src/demo/components/FilterableEditor.test.js
+++ src/demo/components/FilterableEditor.test.js
@@ -28,14 +28,53 @@ describe("FilterableEditor", () => {
   it("renders", () => {
     // Do not snapshot the Draft.js editor, as it contains unstable keys in the content.
     expect(
-      shallow(<FilterableEditor filtered={false} />).find(".EditorToolbar"),
+      shallow(<FilterableEditor filtered={false} extended={false} />).find(
+        ".EditorToolbar",
+      ),
     ).toMatchSnapshot()
+  })
+
+  describe("#extended", () => {
+    it("works", () => {
+      expect(
+        shallow(<FilterableEditor filtered={false} extended />).find(
+          ".EditorToolbar",
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it("save feature", () => {
+      global.sessionStorage.getItem.mockReturnValue(
+        JSON.stringify({
+          entityMap: {},
+          blocks: [
+            {
+              key: "a",
+              text: "test",
+            },
+          ],
+        }),
+      )
+
+      expect(
+        shallow(<FilterableEditor filtered={false} extended />)
+          .state("editorState")
+          .getCurrentContent()
+          .getBlockMap()
+          .map((b) => b.getText())
+          .toJS(),
+      ).toEqual({
+        a: "test",
+      })
+    })
   })
 
   describe("onChange", () => {
     it("works", () => {
       const state = EditorState.createEmpty()
-      const wrapper = shallow(<FilterableEditor filtered={false} />)
+      const wrapper = shallow(
+        <FilterableEditor filtered={false} extended={false} />,
+      )
 
       wrapper.instance().onChange(state)
 
@@ -44,7 +83,7 @@ describe("FilterableEditor", () => {
 
     it("#filtered", () => {
       const state = EditorState.createEmpty()
-      const wrapper = shallow(<FilterableEditor filtered />)
+      const wrapper = shallow(<FilterableEditor filtered extended={false} />)
 
       wrapper.instance().onChange(state)
 
@@ -60,7 +99,23 @@ describe("FilterableEditor", () => {
         getCurrentContent: () => state.getCurrentContent(),
         getLastChangeType: () => "insert-fragment",
       }
-      const wrapper = shallow(<FilterableEditor filtered />)
+      const wrapper = shallow(<FilterableEditor filtered extended={false} />)
+
+      wrapper.instance().onChange(fakeState)
+
+      expect(lib.filterEditorState).toHaveBeenCalled()
+    })
+
+    it("#filtered shouldFilterPaste #extended", () => {
+      jest.spyOn(lib, "filterEditorState")
+      lib.filterEditorState.mockImplementation((opts, e) => e)
+
+      const state = EditorState.createEmpty()
+      const fakeState = {
+        getCurrentContent: () => state.getCurrentContent(),
+        getLastChangeType: () => "insert-fragment",
+      }
+      const wrapper = shallow(<FilterableEditor filtered extended />)
 
       wrapper.instance().onChange(fakeState)
 
@@ -69,7 +124,7 @@ describe("FilterableEditor", () => {
   })
 
   it("toggleStyle", () => {
-    shallow(<FilterableEditor filtered={false} />)
+    shallow(<FilterableEditor filtered={false} extended={false} />)
       .instance()
       .toggleStyle("BOLD", new Event("mousedown"))
 
@@ -77,7 +132,7 @@ describe("FilterableEditor", () => {
   })
 
   it("toggleBlock", () => {
-    shallow(<FilterableEditor filtered={false} />)
+    shallow(<FilterableEditor filtered={false} extended={false} />)
       .instance()
       .toggleBlock("header-two", new Event("mousedown"))
 
@@ -85,7 +140,7 @@ describe("FilterableEditor", () => {
   })
 
   it("toggleEntity - LINK", () => {
-    shallow(<FilterableEditor filtered={false} />)
+    shallow(<FilterableEditor filtered={false} extended={false} />)
       .instance()
       .toggleEntity("LINK")
 
@@ -93,7 +148,7 @@ describe("FilterableEditor", () => {
   })
 
   it("toggleEntity - IMAGE", () => {
-    shallow(<FilterableEditor filtered={false} />)
+    shallow(<FilterableEditor filtered={false} extended={false} />)
       .instance()
       .toggleEntity("IMAGE")
 
@@ -102,13 +157,33 @@ describe("FilterableEditor", () => {
 
   it("blockRenderer", () => {
     expect(
-      shallow(<FilterableEditor filtered={false} />)
+      shallow(<FilterableEditor filtered={false} extended={false} />)
         .instance()
         .blockRenderer({
           getType: () => "atomic",
         }),
     ).toMatchObject({
       editable: false,
+    })
+  })
+
+  describe("onTab", () => {
+    it("works", () => {
+      const wrapper = shallow(
+        <FilterableEditor filtered={false} extended={false} />,
+      )
+
+      wrapper.instance().onChange = jest.fn()
+      wrapper.instance().onTab({})
+      expect(wrapper.instance().onChange).toHaveBeenCalled()
+    })
+
+    it("#extended", () => {
+      const wrapper = shallow(<FilterableEditor filtered={false} extended />)
+
+      wrapper.instance().onChange = jest.fn()
+      wrapper.instance().onTab({})
+      expect(wrapper.instance().onChange).toHaveBeenCalled()
     })
   })
 })
