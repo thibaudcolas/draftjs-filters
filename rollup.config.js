6b02ff7cd47d504b75dfee22bf2f18300c96970d
Thibaud Colas <thibaudcolas@gmail.com>
2018-09-27 22:40:12 +0300

    feat(api): publish package with Flow annotations built in
    BREAKING CHANGE: The Draft.js filters now come with [Flow](https://flow.org/) annotations. For projects that do not use Flow this has no impact. For Flow users, please make sure you correctly type your code so Flow doesn't raise issues because of conflicts with this package's type definitions.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import babel from "rollup-plugin-babel"
 import pkg from "./package.json"
 
+const BANNER = `// @flow
+/*:: import type { ContentState } from "draft-js"*/`
+
 export default [
   {
     input: "src/lib/index.js",
     external: ["draft-js"],
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs", banner: BANNER },
+      { file: pkg.module, format: "es", banner: BANNER },
     ],
     plugins: [
       babel({
@@ -20,8 +23,8 @@ export default [
               modules: false,
             },
           ],
-          "flow",
         ],
+        plugins: ["transform-flow-comments"],
       }),
     ],
   },
