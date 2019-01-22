ef4eb5205158951e9d59962b6f899213064536a0
Thibaud Colas <thibaudcolas@gmail.com>
2019-01-22 20:46:25 +0200

    chore(release): make sure released code is Flow compatible

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import babel from "rollup-plugin-babel"
 import pkg from "./package.json"
 
-const BANNER = `// @flow
-/*:: import type { ContentState } from "draft-js"*/`
+const BANNER = `// @flow`
+const CJS_BANNER = `${BANNER}
+/*:: import type { ContentState, EditorState } from "draft-js"*/`
 
 export default [
   {
     input: "src/lib/index.js",
     external: ["draft-js"],
     output: [
-      { file: pkg.main, format: "cjs", banner: BANNER },
+      { file: pkg.main, format: "cjs", banner: CJS_BANNER },
       { file: pkg.module, format: "es", banner: BANNER },
     ],
     plugins: [
