85d66b726b4e064f627880e81b14d9dde31aa438
Thibaud Colas <thibaudcolas@gmail.com>
2019-06-29 16:06:50 +0100

    chore(qa): add support for running Danger locally

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,7 @@
 // @flow
 // flowlint untyped-import:off
 const { danger, message, warn, fail, schedule } = require("danger")
+const isLocal = !danger.github
 
 const libModifiedFiles = danger.git.modified_files.filter(
   (path) => path.startsWith("src/lib") && path.endsWith("js"),
@@ -8,33 +9,48 @@ const libModifiedFiles = danger.git.modified_files.filter(
 const hasLibChanges =
   libModifiedFiles.filter((filepath) => !filepath.endsWith("test.js")).length >
   0
-const pastingTestsChanges = danger.git.modified_files.filter((p) =>
-  p.startsWith("src/tests"),
-)
+const hasLibTestChanges =
+  libModifiedFiles.filter(
+    (filepath) =>
+      filepath.endsWith("test.js") || filepath.endsWith("test.js.snap"),
+  ).length > 0
 const hasREADMEChanges = danger.git.modified_files.includes("README.md")
 
-// Fails if the description is too short.
-if (!danger.github.pr.body || danger.github.pr.body.length < 10) {
-  fail(":grey_question: This pull request needs a description.")
-}
+if (!isLocal) {
+  const hasLabels = danger.github.issue.labels.length !== 0
+  const isEnhancement =
+    danger.github.issue.labels.some((l) => l.name === "enhancement") ||
+    danger.github.pr.title.includes("feature")
+  const isBug =
+    danger.github.issue.labels.some((l) => l.name === "bug") ||
+    danger.github.pr.title.includes("fix") ||
+    danger.github.pr.title.includes("bug")
 
-// Warns if the PR title contains [WIP]
-const isWIP = danger.github.pr.title.includes("WIP")
-if (isWIP) {
-  const title = ":construction_worker: Work In Progress"
-  const idea =
-    "This PR appears to be a work in progress, and may not be ready to be merged yet."
-  warn(`${title} - <i>${idea}</i>`)
+  if (!hasLabels) {
+    message("What labels should we add to this PR?")
+  }
+
+  // Fails if the description is too short.
+  if (!danger.github.pr.body || danger.github.pr.body.length < 10) {
+    fail(":grey_question: This pull request needs a description.")
+  }
+
+  if (hasLibChanges && !hasLibTestChanges && (isEnhancement || isBug)) {
+    message("This PR may require new test cases")
+  }
+
+  // Warns if the PR title contains [WIP]
+  const isWIP = danger.github.pr.title.includes("WIP")
+  if (isWIP) {
+    const title = ":construction_worker: Work In Progress"
+    const idea =
+      "This PR appears to be a work in progress, and may not be ready to be merged yet."
+    warn(`${title} - <i>${idea}</i>`)
+  }
 }
 
 if (hasLibChanges && !hasREADMEChanges) {
   warn("This pull request updates the library. Should the docs be updated?")
-}
-
-if (pastingTestsChanges.length > 0) {
-  if (pastingTestsChanges.filter((p) => p.endsWith("snap"))) {
-    warn("This PR may be introducing changes to the filtering")
-  }
 }
 
 const hasPackageChanges = danger.git.modified_files.includes("package.json")
@@ -64,6 +80,12 @@ schedule(async () => {
     if (removed.length) {
       const deps = danger.utils.sentence(removed.map((d) => linkDep(d)))
       message(`:tada:, removing dependencies: ${deps}`)
+    }
+
+    if (added.includes("draft-js")) {
+      warn(
+        ":scream: this PR updates Draft.js! Please make sure to review the Draft.js CHANGELOG.",
+      )
     }
   }
 })
