2bd1aaea22eec508772df2a50567f990a540fe1c
Thibaud Colas <thibaudcolas@gmail.com>
2020-01-03 01:20:07 +0000

    chore(qa): fix git hooks using the wrong DRAFTJS_VERSION values

--- .githooks/pre-commit.8.test.sh
+++ .githooks/pre-commit.8.test.sh
@@ -4,6 +4,6 @@ set -e
 
 if [ -n "$JS_STAGED" ] || [ -n "$SNAPSHOT_STAGED" ];
 then
-  DRAFTJS_VERSION=0.10.5 npm run test -s
-  DRAFTJS_VERSION=0.11.2 npm run test -s
+  DRAFTJS_VERSION=0.10 npm run test -s
+  DRAFTJS_VERSION=0.11 npm run test -s
 fi
