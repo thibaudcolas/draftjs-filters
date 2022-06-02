2eb9ddb965103e27642ba454abe7fd6ba2464316
Thibaud Colas <thibaudcolas@gmail.com>
2022-06-02 15:37:12 +0100

    chore(build): set up building of TypeScript type definitions

--- .githooks/pre-commit.6.lint.sh
+++ .githooks/pre-commit.6.lint.sh
@@ -3,5 +3,5 @@
 if [ -n "$JS_STAGED" ];
 then
   npx eslint $JS_STAGED
-  npx documentation lint $JS_STAGED
+  npx documentation lint 'src/**/*.(ts|js|tsx)'
 fi
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,21 @@
 import pkg from "./package.json"
 import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
 
 const config = [
   {
-    input: "src/lib/index.ts",
+    input: "./src/lib/index.ts",
     external: ["draft-js"],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
     ],
     plugins: [typescript()],
+  },
+  {
+    input: "./src/lib/index.ts",
+    output: [{ file: pkg.types, format: "es" }],
+    plugins: [dts()],
   },
 ]
 
